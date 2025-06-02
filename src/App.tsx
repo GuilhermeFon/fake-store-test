00cdc8f8d5b0f6bfbbf0c1f71f863f89e8ea687d
Guilherme Fonseca <guilhermelimafonseca@gmail.com>
2025-06-02 00:13:19 -0300

    feat: integrate React Router for navigation and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,14 @@
-import {useEffect, useState} from "react";
-import "./App.css";
-import Header from "./components/Header";
-import MensCarousel from "./components/MensCarousel";
-import api from "./services/api";
-import ProductsList from "./components/ProductsList";
-import ViewBar from "./components/ViewBar";
-import type {Product} from "./types/Product";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [viewMode, setViewMode] = useState<"list" | "grid">("list");
-  const [itemsPerPage, setItemsPerPage] = useState(5);
-  const [searchTerm, setSearchTerm] = useState("");
-
-  useEffect(() => {
-    api
-      .get("/products")
-      .then((response) => setProducts(response.data))
-      .catch((error) => {
-        console.error("Erro ao buscar produtos:", error);
-      });
-  }, []);
-
-  const filteredProducts = products.filter((product) =>
-    product.title?.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
   return (
     <>
-      <Header onSearch={setSearchTerm} />
-      <ViewBar
-        viewMode={viewMode}
-        setViewMode={setViewMode}
-        itemsPerPage={itemsPerPage}
-        setItemsPerPage={setItemsPerPage}
-      />
-      <MensCarousel products={filteredProducts} />
-      <ProductsList
-        products={filteredProducts}
-        viewMode={viewMode}
-        itemsPerPage={itemsPerPage}
-      />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/produto/:id" element={<ProductDetail />} />
+      </Routes>
     </>
   );
 }
